25ae85cbde5117a45d494a8fbc74acaeca29e5c9
Marcin Jarmułowski <m.jarmulowski@wp.pl>
2017-07-02 23:56:21 +0200

    Add demo for autorization users

---
+++ DemoAuthorizationWebApi/DemoAuthorizationWebApi/Startup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security.OAuth;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(DemoAuthorizationWebApi.Startup))]
+
+namespace DemoAuthorizationWebApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            //enable cros orgin requests
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+
+            var provider = new AuthorizationServerProvider();
+            OAuthAuthorizationServerOptions OAuthOptions = new OAuthAuthorizationServerOptions
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/Token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(15),
+                Provider = provider
+            };
+            app.UseOAuthAuthorizationServer(OAuthOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+        }
+    }
+}
